e873942461ee9f20412e3bc8a4b17115f5ae5827
Muhire-Josue <rutayisire.josue09@gmail.com>
2020-10-12 15:41:18 +0200

    complete server

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const { randomBytes } = require('crypto');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.json());
 const posts = {};
 
 app.get('/posts', (req, res) => {
@@ -9,7 +11,13 @@ app.get('/posts', (req, res) => {
 });
 
 app.post('posts', (req, res) => {
+    const id = randomBytes(4).toString('hex');
+    const { title } = req.body;
+    posts[id] = {
+        id, title
+    };
 
+    res.status(201).send(posts[id]);
 });
 
 app.listen(4000, () => console.log('Server running on port 4000...'));
