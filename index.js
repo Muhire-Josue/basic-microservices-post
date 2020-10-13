72a76c660ab186d4eea30e5901efef45c627c833
Muhire-Josue <rutayisire.josue09@gmail.com>
2020-10-13 12:50:21 +0200

    add morgan

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const { randomBytes } = require('crypto');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const morgan = require('morgan');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
+app.use(morgan('dev'));
 
 const posts = {};
 
